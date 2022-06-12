817c2f30d1d61ee17540a395c429442b286f5ddf
DESKTOP-O5DEB4K\moroz <morozovars@yandex.ru>
2022-06-12 15:39:07 +0300

    updated project arcitecture: docs/IMFD_FW.xlsx; added papers about used method FFT+SFM: docs/FFT_SFM/* removed not used source files.

--- sources/application/cpu/dma/stm32/g4_family/dma_stm32g4.h
+++
@@ -1,9 +0,0 @@
-#ifndef DMA_STM32G4_H_
-#define DMA_STM32G4_H_
-#ifdef __STM32G474_SUBFAMILY 
-  #include "g474_subfamily/dma_stm32g474.h"
-#else
-  #error "stm32 subfamily is undefined"
-#endif
-
-#endif // ! SPI_STM32G4_H_
